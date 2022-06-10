74156ef14a74a6738abc1196852e971595b2a787
Subhomoy Haldar <hungrybluedev@gmail.com>
2022-06-10 21:12:48 +0530

    add license info

--- dist/switch.js
+++ dist/switch.js
@@ -1,3 +1,8 @@
+/* @hungrybluedev/theme-switcher
+ * -----------------------------
+ * This project is made available under the MIT license.
+ * See https://github.com/hungrybluedev/theme-switcher/ for more information.
+ */
 export const attemptToSetDefaultTheme = (theme) => {
   const actualTheme = localStorage.getItem("tswitch-theme") || theme;
   applyTheme(actualTheme);
