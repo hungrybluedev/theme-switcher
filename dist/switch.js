36015512ac90f7d30df149fb4edbca05c8ab9907
Subhomoy Haldar <hungrybluedev@gmail.com>
2022-06-12 15:55:54 +0530

    combine into one file

--- dist/switch.js
+++ dist/switch.js
@@ -3,6 +3,7 @@
  * This project is made available under the MIT license.
  * See https://github.com/hungrybluedev/theme-switcher/ for more information.
  */
+// Externally available functions
 export const attemptToSetDefaultTheme = (theme) => {
   const actualTheme = localStorage.getItem("tswitch-theme") || theme;
   applyTheme(actualTheme);
@@ -14,4 +15,25 @@ export const applyTheme = (theme) => {
 export const getLocalTheme = () => {
   return localStorage.getItem("tswitch-theme") || "light";
 };
+// Website-wide side-effect
+const allButtons = document.querySelectorAll(
+  "#theme-switcher-form input[name=theme-choice]"
+);
 applyTheme(getLocalTheme());
+if (allButtons.length > 0) {
+  attemptToSetDefaultTheme("light");
+  allButtons.forEach((button) =>
+    button.addEventListener("change", () => {
+      console.log(button.value.slice(0, -6));
+      applyTheme(button.value.slice(0, -6));
+    })
+  );
+}
+window.addEventListener("pageshow", () => {
+  const currentTheme = getLocalTheme();
+  applyTheme(currentTheme);
+  const themeRadio = document.getElementById(`${currentTheme}-theme`);
+  if (themeRadio) {
+    themeRadio.checked = true;
+  }
+});
